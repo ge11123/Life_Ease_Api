7f1fb2f7e63d11f2052941a73d7d0cb66f0d25af
ge11123 <as8354093@gmail.com>
2024-04-19 13:55:47 +0800

    依環境讀取appsetting.json

--- LifeManager.Api/src/Application/StartUp/ConfigurationExtensions.cs
+++ LifeManager.Api/src/Application/StartUp/ConfigurationExtensions.cs
@@ -4,8 +4,14 @@
 	{
 		public static void AddJsonFile(this IConfigurationBuilder builder)
 		{
+			var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+			// Get the root path of the application
 			var rootPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "src"));
-			builder.AddJsonFile(Path.Combine(rootPath, "appsettings.json"), optional: false, reloadOnChange: true);
+
+			var appsettingsPath = $"appsettings.{environment}.json";
+
+			builder.AddJsonFile(Path.Combine(rootPath, appsettingsPath), optional: false, reloadOnChange: true);
 		}
 	}
 }
