3283214d0b2fc5a7f94587582443e121e09e9685
ge11123 <as8354093@gmail.com>
2024-06-24 16:49:17 +0800

    feat: 修改 todolist response duetime -> duedate

--- LifeManager.Api/src/Application/Queries/Todo/GetTodoResponse.cs
+++ LifeManager.Api/src/Application/Queries/Todo/GetTodoResponse.cs
@@ -6,6 +6,6 @@
 		public string Title { get; set; } = null!;
 		public string? Description { get; set; }
 		public bool IsCompleted { get; set; }
-		public DateTime? DueTime { get; set; }
+		public DateTime? DueDate { get; set; }
 	}
 }
