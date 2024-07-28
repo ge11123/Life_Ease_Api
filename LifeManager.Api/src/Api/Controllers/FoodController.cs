28db849352d99bbf9e2dddb90714682325f57010
ge11123 <as8354093@gmail.com>
2024-07-28 14:52:30 +0800

    feat: 新增餐廳資訊api 架構

---
+++ LifeManager.Api/src/Api/Controllers/FoodController.cs
@@ -0,0 +1,28 @@
+﻿using LifeManage.src.Application.Handlers.Menu;
+using LifeManage.src.Application.Handlers.Restaurant;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeManage.src.Api.Controllers
+{
+	public class FoodController : BaseController
+	{
+		private readonly IMediator _mediator;
+
+		public FoodController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[Route("restaurant")]
+		[HttpPost]
+		public async Task<IActionResult> CreateRestaurant()
+		{
+			return HandleResult(await _mediator.Send(new CreateRestaurantCommand()));
+		}
+	}
+}
---
+++ LifeManager.Api/src/Application/Handlers/Restaurant/CreateRestaurantCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using LifeManage.src.Application.Commands.Interface;
+using LifeManage.src.Application.Handlers.Interface;
+using MediatR;
+
+namespace LifeManage.src.Application.Handlers.Restaurant
+{
+	public record CreateRestaurantCommand() : ICommand<Unit>;
+
+	public class CreateRestaurantCommandHandler : ICommandHandler<CreateRestaurantCommand, Unit>
+	{
+		private readonly IMapper _mapper;
+
+		public CreateRestaurantCommandHandler(IMapper mapper)
+		{
+			_mapper = mapper;
+		}
+
+		public async Task<Unit> Handle(CreateRestaurantCommand command, CancellationToken cancellationToken)
+		{
+
+			return Unit.Value;
+		}
+	}
+}
